1bdb431ffea7cac82e88cb751e9255b2835e9689
l-Bohdan-l <bpapushoj@gmail.com>
2022-11-06 16:38:26 +0200

    2

--- src/components/JobBoard/JobBoard.jsx
+++ src/components/JobBoard/JobBoard.jsx
@@ -1,5 +1,20 @@
-export function JobBoard () {
+import {MdLocationOn} from 'react-icons/md';
+import img from '../../img/person1.jpg';
+export function JobBoard() {
     return (
-        
+        <div>
+            <ul>
+                <li>
+                    <img src={img} alt="company logo" width='329'/>
+                    <div>
+                        <span>stars</span>
+                        <span>posted 2 days ago</span>
+                    </div>
+                    <h2>Arbeitsmediziner/-in / Betriebsmediziner/-in (m/w/d)</h2>
+                    <p>Department name • Allgemeines Krankenhaus der Stadt Wien - AKH</p>
+                    <span><MdLocationOn/> Vienna, Austria</span>
+                </li>
+            </ul>
+        </div>
     )
 }
