a6d0334021bf7dc227bc5c35df0941218e4a4793
l-Bohdan-l <bpapushoj@gmail.com>
2023-01-05 19:36:13 +0200

    router dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import { AppWrapper } from './App.styled';
 import { JobBoard } from './components/JobBoard/JobBoard';
 import {Container} from './components/Container/Container';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import {Home} from './components/Home/Home'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/job-board",
+    element: <JobBoard/>,
+  }
+]);
+
 function App() {
   return (
     <AppWrapper>
-      <Container>        
-        <JobBoard />
+      <Container>  
+        <RouterProvider router={router} />
+        {/* <JobBoard /> */}
       </Container>
     </AppWrapper>
   );
--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import GlobalStyle from './styles/Global.styled';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// import {Home} from './components/Home/Home'
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home/>,
+//   },
+// ]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
+    {/* <RouterProvider router={router} /> */}
   </React.StrictMode>
 );
 
